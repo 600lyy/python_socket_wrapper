b528e80bd100ad7217dc64873ced9b7352853bb0
andy.lu <andy.y.lu@se.atlascopco.com>
2017-12-08 11:01:16 +0100

    editing readme

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # python_socket_wrapper
 A generic python socket wrapper using sockect and queue
+
+use like so:
+
+    from python_socket_wapper import SocketThread
+    class ClientA(SocketThread):
+
+        def __init__(self):
+            self.send_queue = Queue.Queue()
+            self.receive_queue = Queue.Queue()
+            self.socket_thread = SocketThread(
+                                    address=(ip, port),
+                                    send_queue=self.send_queue,
+                                    receive_queue=self.receive_queue,
+                                )
+            self.socket_thread.start()
+            self.socket_thread.join()
+
+        def send(self, msg):
+            self.send_queue.put(msg)
+
+        def receive(self, timeout):
+            self.receive_queue.get(timeout=timeout)
+
+        def disconnect(self):
+            self.socket_thread.stop()
--- sockets.py
+++ sockets.py
@@ -1,32 +1,6 @@
 #!/usr/bin/env python3
 """
 A client Socket and socket thread that can be used for network communication.
-
-use like so:
-
-    from python_socket_wapper import SocketThread
-    class ClientA(SocketThread):
-
-        def __init__(self):
-            self.send_queue = Queue.Queue()
-            self.receive_queue = Queue.Queue()
-            self.socket_thread = SocketThread(
-                                    address=(ip, port),
-                                    send_queue=self.send_queue,
-                                    receive_queue=self.receive_queue,
-                                )
-            self.socket_thread.start()
-            self.socket_thread.join()
-
-        def send(self, msg):
-            self.send_queue.put(msg)
-
-        def receive(self, timeout):
-            self.receive_queue.get(timeout=timeout)
-
-        def disconnect(self):
-            self.socket_thread.stop()
-
 """
 import threading
 import socket
